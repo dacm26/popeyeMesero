be75abc5f6debffb3bf06c5acf67b55ed2160384
Daniel Alejandro Coello Martinez <dacm26@gmail.com>
2015-06-03 12:00:30 -0600

    before adding the API's requests to the controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,16 +1,20 @@
 angular.module('starter.controllers', [])
 
-.controller('EntradaCtrl', function($scope) {})
+.controller('EntradaCtrl', function($scope) {
+  
+})
 
-.controller('PrincipalCtrl', function($scope) {})
+.controller('PrincipalCtrl', function($scope) {
+  
+})
 
-.controller('PostreCtrl', function($scope) {})
+.controller('PostreCtrl', function($scope) {
+  
+})
 
 .controller('BebidaCtrl', function($scope) {
   
 })
 
 .controller('FacturaCtrl', function($scope) {
-
 });
-
